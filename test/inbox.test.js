105dd032ba6ecbaff4572eb0a2694395329dbf88
mantonoiu <antonoiumadalin@gmail.com>
2021-03-10 08:51:14 +0000

    👑 MOCHA BOILERPLATE FOR TESTING - WATCH ME ( refactored with async/await)

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,16 +3,18 @@ const ganache = require('ganache-cli')
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
-    //1. Get a list of all accounts
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts);
-        })
+// Declare it outside of beforeEach to be global 
+let accounts;
 
-    //2. Use one to deploy the contract
+beforeEach(async () => {
+    //1. Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+    //2. Use one acc. to deploy the contract
 })
 
 describe('Inbox Contract', () => {
-    it('logs a list of fetched accounts', () => { })
+    it('logs a list of fetched accounts', () => {
+        console.log(accounts);
+    })
 })
