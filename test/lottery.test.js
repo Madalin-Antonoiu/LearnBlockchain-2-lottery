06b1e0c41c53b1e4786b7c614d2fa86431271469
mantonoiu <antonoiumadalin@gmail.com>
2021-03-11 11:53:25 +0000

    Tests - allows one & multiple addresses to enter (passing)

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -19,5 +19,42 @@ describe('Lottery Contract', () => {
     it('it deploys a contract', () => {
         assert.ok(lottery.options.address);
     })
+    it('allows one address to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        })
+
+        assert.strictEqual(accounts[0], players[0]);
+        assert.strictEqual(1, players.length); // the value that it should be, the value that it is
+    })
+    it('allows multiple addresses to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+        await lottery.methods.enter().send({
+            from: accounts[1],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+        await lottery.methods.enter().send({
+            from: accounts[2],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        })
+
+        assert.strictEqual(accounts[0], players[0]);
+        assert.strictEqual(accounts[1], players[1]);
+        assert.strictEqual(accounts[2], players[2]);
+        assert.strictEqual(3, players.length); // the value that it should be, the value that it is
+    })
+
 })
 
