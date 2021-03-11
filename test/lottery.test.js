541827dd7cb775b735b3eaf0cba8013a40eca125
mantonoiu <antonoiumadalin@gmail.com>
2021-03-11 13:57:40 +0000

    Test Pass - sends money to the winner and resets the players array

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -70,6 +70,34 @@ describe('Lottery Contract', () => {
             assert(err); // it is ok if errors, cannot send with that value
         }
     })
+    it('only manager  can call pick winner', async () => {
+
+        try {
+
+            await lottery.methods.pickWinner().send({
+                from: accounts[1], // the manager is accounts[0]
+            })
+
+            assert(false); // fail the test if we get to this line of code
+
+        } catch (err) {
+            assert(err); // it is ok if errors, cannot send with that value
+        }
+    })
+    it('sends money to the winner and resets the players array', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('2', 'ether')
+        })
+
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+        await lottery.methods.pickWinner().send({ from: accounts[0] });
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+        const difference = finalBalance - initialBalance;
+
+        assert(difference > web3.utils.toWei('1.8', 'ether'));
+
+    })
 
 })
 
